203ead289974123ab021f1125b52c716c71dec22
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2022-07-02 17:05:34 +0200

    Refactoring of the code

---
+++ api_anagrams.js
@@ -0,0 +1,87 @@
+// Local functions
+function removePunctuation(sentence) {
+    return String(sentence).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+
+function searchAnagramInArray(string, array) {
+    let outcome = [];
+    for (let word of array) {
+        if (areAnagrams(string, word)) {
+            outcome.push(string, word);
+        }
+    }
+    return outcome;
+}
+
+// Exported functions
+function areAnagrams(string1, string2) {
+    if (!string1 || !string2) {
+        return false;
+    }
+
+    let str1 = String(string1).toLocaleLowerCase();
+    let str2 = String(string2).toLocaleLowerCase();
+
+    let len1 = str1.length;
+    let len2 = str2.length;
+
+    if (str1 == str2 || len1 == 1 || len2 == 2 || len1 != len2) {
+        return false;
+    }
+
+    let dictStr1 = {};
+    let dictStr2 = {};
+
+    for (let val of str1) {
+        dictStr1[val] = (dictStr1[val] || 0) + 1;
+    }
+
+    for (let val of str2) {
+        dictStr2[val] = (dictStr2[val] || 0) + 1;
+    }
+
+    for (let key in dictStr1) {
+        if (dictStr1[key] != dictStr2[key])
+            return false;
+    }
+
+    return true;
+}
+
+function searchAnagramInSentence(string, sentence) {
+    if (!string || !sentence) {
+        return false;
+    }
+    sentence = removePunctuation(sentence);
+    let words = sentence.split(" ");
+    let outcome = searchAnagramInArray(string, words);
+
+    return outcome;
+}
+
+function anagramsInSentence(sentence) {
+    sentence = removePunctuation(sentence);
+
+    const possibleAnagrams = sentence.split(" ");
+    let words = sentence.split(" ");
+
+    let outcome = [];
+
+    for (let possibleAnagram of possibleAnagrams) {
+        let anagrams = searchAnagramInArray(possibleAnagram, words);
+
+        if (!(anagrams.length == 0)) {
+            words = words.filter(x => !anagrams.includes(x));
+            possibleAnagram = words.filter(x => !anagrams.includes(x));
+            outcome.push(anagrams);
+        }
+    }
+
+    return outcome;
+}
+
+module.exports = {
+    areAnagrams,
+    searchAnagramInSentence,
+    anagramsInSentence
+}
--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
-const {
-    anagrams
-} = require('./anagrams')
-const tools = require('./utility')
+const api_anagrams = require('./api_anagrams')
 
 app.use(express.json());
 
@@ -11,7 +8,7 @@ app.post('/A/', (req, res) => {
     let keys = Object.keys(req.body);
 
     if (keys.length == 2) {
-        let success = tools.areAnagrams(req.body[keys[0]], req.body[keys[1]]);
+        let success = api_anagrams.areAnagrams(req.body[keys[0]], req.body[keys[1]]);
         res.status(200).json({
             outcome: success
         });
@@ -25,13 +22,27 @@ app.post('/A/', (req, res) => {
 app.post('/B/', (req, res) => {
     let keys = Object.keys(req.body);
     if (keys.length == 2) {
-        let anagrams = tools.anagramsInSentence(req.body[keys[0]], req.body[keys[1]]);
+        let anagrams = api_anagrams.searchAnagramInSentence(req.body[keys[0]], req.body[keys[1]]);
         res.status(200).json({
             outcome: anagrams
         });
     } else {
         res.status(400).json({
-            outcome: false
+            outcome: "Wrong number of elements."
+        });
+    }
+});
+
+app.post('/C/', (req, res) => {
+    let keys = Object.keys(req.body);
+    if (keys.length == 1) {
+        let arrayOfAnagrams = api_anagrams.anagramsInSentence(req.body[keys[0]]);
+        res.status(200).json({
+            outcome: arrayOfAnagrams
+        });
+    } else {
+        res.status(400).json({
+            outcome: "Wrong number of elements."
         });
     }
 });
